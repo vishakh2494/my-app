c54820c5349a2acc01400b7eb9784779d9fb5f83
vishakh2494 <vlakshm2@mail.depaul.edu>
2018-08-24 19:30:08 +0000

    git commit

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -3,6 +3,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Vishakh!" );
+        System.out.println( "Hello Vishakh & Kartik!" );
     }
 }
